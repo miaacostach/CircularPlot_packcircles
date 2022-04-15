bf5f7a3ea49f86f97f4cc724b5a2e54bb8e469eb
Miguel Acosta <miaacostach@una.edu.co>
2022-04-15 02:09:07 -0500

    Initial script

---
+++ packcircles.R
@@ -0,0 +1,63 @@
+# CARGAR LIBRERIAS ----------------------------------------------------
+library(readxl)
+library(Rcpp)
+library(arrow)
+library(packcircles)
+library(viridis)
+library(ggplot2)
+library(tidyr)
+
+
+# CARGAR BASE DATOS---------------------------------------------------
+
+#file.choose()
+d<-read_excel("C:\\Users\\MiguelAngel\\Documents\\R Miguelo\\Guillermo (Circular Packing)\\oms.xls")
+
+head(d)
+View(d)
+
+# VERIFICAR [NAs] ------------------------------------------------------------
+
+sapply(d, function(x) sum(is.na(x)))
+
+d2 <- d[!is.na(d$Siglas),]
+
+sapply(d2, function(x) sum(is.na(x)))
+
+
+# CONSTRUCCION DE GRÁFICO -----------------------------------------------
+
+d3 <- circleProgressiveLayout(d2$Frecuencia, sizetype='area')
+
+
+d4 <- cbind(d2$Siglas, d2$Frecuencia , d3)
+
+names(d4)
+
+names(d4)[1] = "Siglas"
+names(d4)[2] = "Frecuencia"
+
+names(d4)
+
+
+# CREACION DE VERTICES -----------------------------------------------
+
+d5 <- circleLayoutVertices(d3, npoints=30)
+head(d5)
+
+
+# GRAFICO CIRCULAR --------------------------------------------------------
+
+ggplot() + 
+  geom_polygon(data = d5, aes(x, y, group = id, fill=as.factor(id)), 
+               colour = "gray8",size=0.34, alpha = 0.65) +
+  geom_text(data = d4, aes(x, y, size=Frecuencia, label=Siglas),size=2.0) +
+  scale_size_continuous(range = c(3,5)) +
+  theme_void() + 
+  theme(legend.position="none",
+        plot.background = element_rect(fill="gray93"),
+        plot.title = element_text(color="black")) +
+  coord_equal()+
+  ggtitle("Participacion organizaciones sindicales en Mexico")
+
+
